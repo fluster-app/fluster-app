11c415020d53a80723724d482369cef37ba3cb00
daviddalbusco <david.dalbusco@outlook.com>
2018-09-29 17:13:36 +0200

    feat(#25): add link to step limitation once ad published

--- src/app/pages/advertise/new-ad/new-ad.page.ts
+++ src/app/pages/advertise/new-ad/new-ad.page.ts
@@ -242,6 +242,12 @@ export class NewAdPage extends AbstractPage implements OnInit {
         await this.enableMenu(this.menuController, false, true);
     }
 
+    async navigateToAdminLimitation() {
+        this.navParamsService.setAdminAppointmentsNavParams({menuToggle: true});
+        await this.navController.navigateRoot('/admin-limitation', true);
+        await this.enableMenu(this.menuController, false, true);
+    }
+
     // HACK: Fck it, Load incrementaly these steps for devices with small memory which could not handle a important load on load of the slides
 
     loadNextSlidePrice() {
